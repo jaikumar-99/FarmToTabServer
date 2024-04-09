7b3ccff456b5d932ca56b0f9f55b3da83bd804bf
jaikumar-99 <jaikumarg888@gmail.com>
2024-04-09 14:00:09 +0530

    Encyrption & Decryption Added!

--- src/controllers/shared.service.js
+++ src/controllers/shared.service.js
@@ -1,7 +1,47 @@
+import CryptoJS from "crypto-js";
+
 export const parseBody = async (body) => {
-  return body;
+  console.log(body);
+  const result = await decryptService(body.cheese);
+  return result;
 };
 
 export const parseOutput = async (output) => {
-  return output;
+  const result = await encryptService(output);
+  console.log({ cheese: result });
+  return { cheese: result };
+};
+
+// ecnrypt data using nodejs crypto module
+export const encryptService = async (dataToEncrypt) => {
+  console.log("in encryption");
+
+  try {
+    const encryptedText = CryptoJS.AES.encrypt(
+      JSON.stringify(dataToEncrypt),
+      process.env.hashkey
+    ).toString();
+    // console.log(encryptedText);
+    return encryptedText;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+};
+
+// decrypt data using nodejs crypto module
+export const decryptService = async (dataToDecrypt) => {
+  console.log("in decryption");
+
+  try {
+    const bytes = CryptoJS.AES.decrypt(dataToDecrypt, process.env.hashkey);
+    console.info(JSON.parse(bytes.toString(CryptoJS.enc.Utf8)));
+    const decryptedText = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
+    // console.log('\n\nDecoded String', decryptedText);
+
+    return decryptedText;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
 };
